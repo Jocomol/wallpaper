3db0fbb4ad4cbd15acdf23fbaea9ec24e81fdf74
Jocomol <joelmeier08@gmail.com>
2020-12-27 19:27:41 +0100

    fixed smth

--- setup.sh
+++ setup.sh
@@ -3,4 +3,5 @@ wallpaper_path="/home/joco/.wallpapers/"
 cp prepare.sh $wallpaper_path
 cp wallpaper ~/.local/bin
 $wallpaper_path/prepare.sh
+rm $wallpaper_path/prepare.sh
 echo "Don't forget to add alias wallpaper=\". wallpaper\" to your .bash_aliases"
