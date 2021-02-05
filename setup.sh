9052f2ba5e323f2fa94cd8b2ff5301fd03ce81f0
Jocomol <joelmeier08@gmail.com>
2021-02-05 10:43:32 +0100

    fixed minor bug

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 wallpaper_path="/home/joco/.wallpapers/"
 cp prepare.sh $wallpaper_path
-cp wallpaper ~/.local/bin
+cp wallpaper ~/.local/bin/
 $wallpaper_path/prepare.sh
 rm $wallpaper_path/prepare.sh
 echo "Don't forget to add alias wallpaper=\". wallpaper\" to your .bash_aliases"
